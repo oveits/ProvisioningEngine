87b99c85d4c45bfdc26d47bf2b1d3367f4a135e8
Oliver Veits <oliver.veits@web.de>
2014-10-29 06:16:39 +0100

    changed app/models/provisioning.rb: 'targetobject.destroy unless targetobject == customer'; added sleep 2 to startWebPortal.sh/startDelayedJobs.sh

--- startDelayedJobs.sh
+++ startDelayedJobs.sh
@@ -2,6 +2,7 @@ export HTTP_PROXY=
 export http_proxy=
 
 ~/.rbenv/shims/rake jobs:work >> /var/log/WebPortal.log &
+  sleep 2
   myDelayedJobsPid=`ps -ef | grep "rake jobs:work" | grep -v grep | grep -v shims | awk '{print $2}'`
   echo "Delayed Jobs started: PID=$myDelayedJobsPid"
   echo "Delayed Jobs started: PID=$myDelayedJobsPid" >> /var/log/WebPortal.log
--- startWebPortal.sh
+++ startWebPortal.sh
@@ -2,7 +2,7 @@ export HTTP_PROXY=
 export http_proxy=
 
 ~/.rbenv/shims/rails s -p 80 >> /var/log/WebPortal.log &
-  sleep 1
+  sleep 2
   myWebPortalPid=`ps -ef | grep "rails s" | grep -v grep | grep -v shims | awk '{print $2}'`
   echo "Web Portal started: PID=$myWebPortalPid"
   echo "Web Portal started: PID=$myWebPortalPid" >> /var/log/WebPortal.log
