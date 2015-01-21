8133593dcdd57fe2e42dd6459b3bf513d9082deb
Oliver Veits <oliver.veits@web.de>
2015-01-21 01:08:46 +0100

    moved to unobstrusive javascript for page reload app/assets/javascripts/page_reload.js and moved to ajax for customers show pages (no reload on this path anymore); not fully ready, since site's status and flash are not updated yet

---
+++ app/assets/javascripts/page_reload.js
@@ -0,0 +1,28 @@
+  var mySetReload, pattern, regex;
+
+
+  //pattern = "/customers$|customers/[1-9][0-9]*$|sites$|sites/[1-9][0-9]*$|users$|users/[1-9][0-9]*$|provisionings$|provisionings/[1-9][0-9]*$";
+  // same without customers/[1-9][0-9]
+  pattern = "/customers$|sites$|sites/[1-9][0-9]*$|users$|users/[1-9][0-9]*$|provisionings$|provisionings/[1-9][0-9]*$";
+  //pattern = "/customers$|customers/[1-9][0-9]*$|sites$|sites/[1-9][0-9]*$" //|customers/[1-9][0-9]*$|sites$|sites/[1-9][0-9]*$|users$|users/[1-9][0-9]*$|provisionings$||provisionings/[1-9][0-9]*$";
+  regex = RegExp("" + pattern);
+
+  mySetReload = function() {
+    if (regex.test(window.location.pathname)) {
+      if (window.myRefresh != null) {
+        clearTimeout(window.myRefresh);
+      }
+      return window.myRefresh = setTimeout("location.reload(true);", 10000);
+      //alert(window.myRefresh);
+    } else {
+      if (window.myRefresh != null) {
+        clearTimeout(window.myRefresh);
+      }
+      //alert(window.myRefresh);
+      return window.myRefresh = null;
+    }
+  };
+
+  $(document).ready(mySetReload);
+
+  $(document).on('page:load', mySetReload);
