6314ac464ddf1302590a0bd60c6196717de8db0e
Oliver Veits <oliver.veits@web.de>
2015-02-05 04:20:49 +0100

    omit running web service as root; needed to make sure /var/log/Webportal.log is writable by user provisioningengine

--- startWebPortal_development.sh
+++ startWebPortal_development.sh
@@ -2,6 +2,8 @@ export HTTP_PROXY=
 export http_proxy=
 export WEBPORTAL_BASEURL="/dev"
 
+[ -w /var/log/WebPortal.log ] || ( sudo touch /var/log/WebPortal.log; sudo chown provisioningengine:provisioningengine /var/log/WebPortal.log  )
+
 ~/.rbenv/shims/rails s -p 3001 >> /var/log/WebPortal.log &
   sleep 2
   myWebPortalPid=`ps -ef | grep "rails s" | grep -v grep | grep -v shims | awk '{print $2}'`
