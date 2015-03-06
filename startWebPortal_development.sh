676fdcf232ec41838ec2dc0dfad4f86eee0179a5
Oliver Veits <oliver.veits@web.de>
2015-03-06 15:31:01 +0100

    added @myparams in controllers->update; in sidebar, changed titles to non-links; in app/views/shared/_edit_button.html.erb, changed the behavior of the button: it is non-executable while the status is 'waiting' or 'in progress'

--- startWebPortal_development.sh
+++ startWebPortal_development.sh
@@ -5,8 +5,8 @@ export WEBPORTAL_BASEURL="/dev"
 [ -w /var/log/WebPortal.log ] || ( sudo touch /var/log/WebPortal.log; sudo chown provisioningengine:provisioningengine /var/log/WebPortal.log  )
 
 #~/.rbenv/shims/rails s -e 'production' -p 3001 >> /var/log/WebPortal.log &
-~/.rbenv/shims/rails s -p 3001 >> /var/log/WebPortal.log &
+~/.rbenv/shims/rails s -p 3001 >> /var/log/WebPortal.log 2>&1 &
   sleep 2
   myWebPortalPid=`ps -ef | grep "rails s" | grep -v grep | grep -v shims | awk '{print $2}'`
   echo "Web Portal started: PID=$myWebPortalPid"
-  echo "Web Portal started: PID=$myWebPortalPid" >> /var/log/WebPortal.log
+  echo "Web Portal started: PID=$myWebPortalPid" >> /var/log/WebPortal.log 2>&1
