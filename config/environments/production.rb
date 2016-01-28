8cfd553211e93e37ec603215115e34e160d0c756
oveits <oliver.veits@web.de>
2016-01-28 10:44:47 +0000

    config/environments/production.rb: changed from serve_static_assets to config.serve_static_files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
 
 # OV changed to true; see http://stackoverflow.com/questions/21969549/rails-application-css-asset-not-found-in-production-mode
   #config.serve_static_assets = true
-  config.serve_static_assets = false
+  config.config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
