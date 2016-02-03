eca8b5f7688b0acdb9dbe82bee6c93c01bcef18f
oveits <oliver.veits@web.de>
2016-02-03 15:51:52 +0000

    rails generate active_admin:install
    for details see https://oliverveits.wordpress.com/2016/02/02/rails-active-admin-installation-experiences-howto/

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :text_documents
 
   #root 'welcome#index'
@@ -153,4 +155,3 @@ scope(path: baseURL) do
   #   end
 end # scope(path: baseURL) do
 end # Rails.application.routes.draw do
-
