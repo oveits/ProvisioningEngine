70d9277536c9253d8bed201c9c7a0be85ffb48ac
oveits <oliver.veits@web.de>
2016-02-05 17:06:25 +0000

    cleaning: removing config model and database table entry

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160205102214) do
+ActiveRecord::Schema.define(version: 20160205121053) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -45,17 +45,6 @@ ActiveRecord::Schema.define(version: 20160205102214) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-
-  create_table "configs", force: :cascade do |t|
-    t.string   "name"
-    t.string   "value_type"
-    t.string   "value"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.string   "short_description"
-    t.text     "description"
-    t.string   "default_value"
-  end
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
