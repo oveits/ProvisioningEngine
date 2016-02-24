841bbb1db8ba924487eec712039f70d23144fd78
Oliver Veits <oliver.veits@web.de>
2016-02-24 18:24:56 +0100

    added table to admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,12 +3,12 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+#    div class: "blank_slate_container", id: "dashboard_default_message" do
+#      span class: "blank_slate" do
+#        span I18n.t("active_admin.dashboard_welcome.welcome")
+#        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+#      end
+#    end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
@@ -22,6 +22,13 @@ ActiveAdmin.register_page "Dashboard" do
     #       end
     #     end
     #   end
+
+  #panel "System Settings" do
+    table_for SystemSetting.where(value_type: 'boolean') do |t|
+      t.column("System Setting") { |system_setting| system_setting.name }
+      t.column("Value")   { |system_setting| system_setting.value }
+    end
+  #end
 
     #   column do
     #     panel "Info" do
