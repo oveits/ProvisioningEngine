459dce4d9bd063a4795eb9c315b6bc22848db71c
Oliver Veits <oliver.veits@web.de>
2016-10-07 19:42:10 +0200

    Improvements of start scripts
    startWebportal.sh and startDelayedJobs.sh now will start
    the processes only, if they are not already running.

--- startDelayedJobs.sh
+++ startDelayedJobs.sh
@@ -1,11 +1,20 @@
+#!/usr/bin/env bash
+
 export HTTP_PROXY=
 export http_proxy=
 
 [ -w /var/log/WebPortal.log ] || ( sudo touch /var/log/WebPortal.log; sudo chown provisioningengine:provisioningengine /var/log/WebPortal.log  )
 
-cd `dirname $0`
-./status | grep "Delayed Jobs: running" || ~/.rbenv/shims/rake jobs:work >> /var/log/WebPortal.log 2>&1 &
+myDelayedJobsPid=`ps -ef | grep "rake jobs:work" | grep -v grep | grep -v shims | awk '{print $2}'`
+[ "$myDelayedJobsPid" == "" ] && ~/.rbenv/shims/rake jobs:work | tee -a /var/log/WebPortal.log 2>&1 &
 sleep 2
 myDelayedJobsPid=`ps -ef | grep "rake jobs:work" | grep -v grep | grep -v shims | awk '{print $2}'`
-echo "Delayed Jobs started: PID=$myDelayedJobsPid"
-echo "Delayed Jobs started: PID=$myDelayedJobsPid" >> /var/log/WebPortal.log 2>&1
+if [ "$myDelayedJobsPid" != "" ]; then
+  echo "Delayed Jobs started: PID=$myDelayedJobsPid"
+  echo "Delayed Jobs started: PID=$myDelayedJobsPid" >> /var/log/WebPortal.log 2>&1
+else
+  message="Could not start Delayed Jobs! See /var/log/WebPortal.log for details."
+  echo "$message" >> /var/log/WebPortal.log 2>&1
+  echo "$message" >&2
+  exit 1
+fi
--- startWebPortal.sh
+++ startWebPortal.sh
@@ -1,10 +1,22 @@
+#!/usr/bin/env bash
+
 export HTTP_PROXY=
 export http_proxy=
 [ -w /var/log/WebPortal.log ] || ( sudo touch /var/log/WebPortal.log; sudo chown provisioningengine:provisioningengine /var/log/WebPortal.log  )
 
-#~/.rbenv/shims/rails s -e 'production' -p 3000 >> /var/log/WebPortal.log &
-~/.rbenv/shims/rails s -p 3000 >> /var/log/WebPortal.log 2>&1 &
+  myWebPortalPid=`ps -ef | grep "rails s" | grep "3000" | grep -v grep | grep -v shims | awk '{print $2}'`
+  [ "$myWebPortalPid" == "" ] && ~/.rbenv/shims/rails s -p 3000 >> /var/log/WebPortal.log 2>&1 &
+                                #~/.rbenv/shims/rails s -e 'production' -p 3000
   sleep 2
   myWebPortalPid=`ps -ef | grep "rails s" | grep "3000" | grep -v grep | grep -v shims | awk '{print $2}'`
+
+if [ "$myWebPortalPid" != "" ]; then
   echo "Web Portal started: PID=$myWebPortalPid"
   echo "Web Portal started: PID=$myWebPortalPid" >> /var/log/WebPortal.log 2>&1
+else
+  message="Could not start Web Portal! See /var/log/WebPortal.log for details."
+  echo "$message" >> /var/log/WebPortal.log 2>&1
+  echo "$message" >&2
+  exit 1
+fi
+
